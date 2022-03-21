4084a37c68dd658db5237935b4fb5bd24ee72322
kotovi4 <kotovi4.m@gmail.com>
2022-03-21 16:47:01 +0300

    fix: create error

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const text = document.querySelector('.text'),
       btn = document.querySelector('.btn'),
-      img = document.querySelector('.img');
+      img = document.querySelector('.img'),
+      container = document.querySelector('.container');
+
+const message = {
+  loading: 'Загрузка...',
+  failure: 'Что-то пошло не так...'
+};
 
 function getData() {
   fetch('https://api.icndb.com/jokes')
@@ -8,6 +14,12 @@ function getData() {
     .then((data) => {
       const randomIndex = Math.floor(Math.random() * data.value.length);
       text.textContent = data.value[randomIndex].joke;
+    })
+    .catch((error) => {
+      const errorMessage = document.createElement('div');
+      errorMessage.innerHTML = `${message.failure}`;
+      container.appendChild(errorMessage);
+      console.error(error);
     });
 }
 
